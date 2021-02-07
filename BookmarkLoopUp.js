ddb8d6e7e50c53be4a23149150069d3707f05a52
Vanillj <40024429+Vanillj@users.noreply.github.com>
2021-02-07 17:33:20 +0100

    Update BookmarkLoopUp.js

--- BookmarkLoopUp.js
+++ BookmarkLoopUp.js
@@ -1,12 +1,13 @@
 var ticker = prompt("Enter Ticker", "AAPL");
 
 if (ticker != null & ticker.length > 0) {
-    window.open("https://whalewisdom.com/stock/" + ticker);
+    //window.open("https://whalewisdom.com/stock/" + ticker);
     window.open("https://www.marketwatch.com/investing/stock/" + ticker + "/analystestimates?mod=mw_quote_tab#cx-banner");
-    window.open("https://www.morningstar.com/search?query=" + ticker);
+    //window.open("https://www.morningstar.com/search?query=" + ticker);
     window.open("https://www.di.se/bors/aktier/" + ticker.toLowerCase());
-    window.open("https://www.dataroma.com/m/stock.php?sym=" + ticker);
+    //window.open("https://www.dataroma.com/m/stock.php?sym=" + ticker);
     window.open("https://www.gurufocus.com/stock/" + ticker + "/summary");
+    window.open("https://lazyfa.com/" + ticker + "/dashboard/overview/");
     window.open("https://seekingalpha.com/symbol/" + ticker);
     window.open("https://www.tradingview.com/symbols/" + ticker);
 } 
